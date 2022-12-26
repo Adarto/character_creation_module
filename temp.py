dd2b3b1267436723b74ad7ba0a6a30f6f25a9fee
Алексей Сажнев <a.sazhnev88@gmail.com>
2022-12-27 02:38:54 +0300

    Dockstrings

--- temp.py
+++ temp.py
@@ -1,4 +1,13 @@
-def we_crash_all(name: str) -> str:    
-    return 'Привет, ' + name + ', мы всё сломали!'
+#def we_crash_all(name: str) -> str:    
+#    return 'Привет, ' + name + ', мы всё сломали!'
 
-print(we_crash_all('Наташа'))
+#print(we_crash_all('Наташа'))
+
+import math
+
+# Спросим, что хорошего в этой библиотеке.
+print(print.__doc__)
+
+# Будет напечатано:
+# This module provides access to the mathematical functions
+# defined by the C standard.
